3ff7e8d8aa60f8269874a7dfe52b5919125f059a
Aisheair <ankitkargupta98313@gmail.com>
2024-11-23 17:03:00 +0530

    this

--- main.js
+++ main.js
@@ -4,43 +4,63 @@
 //          object = {key:value,
 //                    function()}
 
-const person1 = {
-    firstName: "spongebob",
-    lastName: "Squarepants",
-    age: 30,
-    isEmplopyed: true,
-    sayHello: function(){console.log("Hi i am spongebob!")},
-    eat: () => {console.log("I am eating a krabby patty")},
+// const person1 = {
+//     firstName: "spongebob",
+//     lastName: "Squarepants",
+//     age: 30,
+//     isEmplopyed: true,
+//     sayHello: function(){console.log("Hi i am spongebob!")},
+//     eat: () => {console.log("I am eating a krabby patty")},
+// }
+
+// const person2 = {
+//     firstName: "Patric",
+//     lastName: "star",
+//     age: 42,
+//     isEmplopyed: false,
+//     sayHello: () => {console.log("Hay i am Patric...")},
+//     eat: function(){console.log("I am eating a roast beef, chicken and pizza")},
+
+// }
+
+
+
+
+
+//  console.log(person1.firstName);
+//  console.log(person1.lastName);
+//  console.log(person1.age);
+//  console.log(person1.isEmplopyed);
+//  person1.sayHello();
+//  person1.eat()
+ 
+//  console.log(person2.firstName);
+//  console.log(person2.lastName);
+//  console.log(person2.age);
+//  console.log(person2.isEmplopyed);
+//  person2.sayHello();
+//  person2.eat()
+ 
+//========================================================================
+
+
+
+// this = reference to the object where THIS is used
+//        (the object depends on the immediate context)
+//         person.name = this.name
+//         dosent work with => 
+
+
+const person1 ={
+    name: "Spongebob",
+    favfood: "hamburgers",
+    sayHello: function () {console.log(`Hi, i am ${this.name}`)},
 }
 
-const person2 = {
-    firstName: "Patric",
-    lastName: "star",
-    age: 42,
-    isEmplopyed: false,
-    sayHello: () => {console.log("Hay i am Patric...")},
-    eat: function(){console.log("I am eating a roast beef, chicken and pizza")},
-
-}
+person1.sayHello();
 
 
-
-
-
- console.log(person1.firstName);
- console.log(person1.lastName);
- console.log(person1.age);
- console.log(person1.isEmplopyed);
- person1.sayHello();
- person1.eat()
- 
- console.log(person2.firstName);
- console.log(person2.lastName);
- console.log(person2.age);
- console.log(person2.isEmplopyed);
- person2.sayHello();
- person2.eat()
- 
+console.log(this);
 
 
 
